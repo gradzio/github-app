83cee190b1c85cf29c172a9e1fb07e32e50f2f0e
gradzio <gradzio@bettercollective.com>
2019-02-04 23:35:19 +0100

    Added searchbox

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, OnInit } from '@angular/core';
+import { Organization } from 'src/app/core/domain/organization/organization.model';
+import { StateService } from 'src/app/core/state/state.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: []
 })
-export class AppHeaderComponent {}
+export class AppHeaderComponent implements OnInit {
+  
+  organization$: Observable<Organization>;
+
+  constructor(private state: StateService) {}
+
+  ngOnInit() {
+    this.organization$ = this.state.selectedOrganization$;
+  }
+
+  search(itemName) {
+    this.state.selectOrganization(itemName);
+  }
+}
